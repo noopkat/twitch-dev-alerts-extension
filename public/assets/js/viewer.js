1961854d07f416ec08e4c394766a373211803b2d
Suz Hinton <suz.hinton@gmail.com>
2019-02-23 18:02:01 -0800

    code cleanup

--- public/assets/js/viewer.js
+++ public/assets/js/viewer.js
@@ -1,167 +1,179 @@
 "use strict";
 
-const _window$preact = window.preact,
-      h = _window$preact.h,
-      render = _window$preact.render,
-      Component = _window$preact.Component;
-const twitch = window.Twitch.ext;
-const eventListSize = 3;
-const assetsDir = 'assets';
-const githubEventNames = {
-  pull_request: 'pull_request',
-  issue: 'issues',
-  check_suite: 'check_suite'
-};
-const githubEventIcons = {
-  pull_request: 'git-pull-request.svg',
-  issue: 'alert-circle.svg',
-  check_pass: 'check-circle.svg',
-  check_fail: 'x-circle.svg'
-};
-/* 
- HELPER FUNCTIONS
-*/
+(function () {
+  const _window$preact = window.preact,
+        h = _window$preact.h,
+        render = _window$preact.render,
+        Component = _window$preact.Component;
+  const twitch = window.Twitch.ext;
+  const assetsDir = 'assets';
+  const imagesDir = 'images';
+  const eventListSize = 3;
+  const animateTimeout = 1000;
+  const githubEventNames = {
+    pull_request: 'pull_request',
+    issue: 'issues',
+    check_suite: 'check_suite'
+  };
+  const githubEventIcons = {
+    pull_request: 'git-pull-request.svg',
+    issue: 'alert-circle.svg',
+    check_pass: 'check-circle.svg',
+    check_fail: 'x-circle.svg'
+  };
+  /* 
+   HELPER FUNCTIONS
+  */
 
-function getEventIcon(event) {
-  let icon;
+  function getEventIcon(event) {
+    let icon;
 
-  switch (event.event_name) {
-    case githubEventNames.check_suite:
-      icon = event.conclusion === 'success' ? githubEventIcons.check_pass : githubEventIcons.check_fail;
-      break;
+    switch (event.event_name) {
+      case githubEventNames.check_suite:
+        icon = event.conclusion === 'success' ? githubEventIcons.check_pass : githubEventIcons.check_fail;
+        break;
 
-    case githubEventNames.pull_request:
-      icon = githubEventIcons.pull_request;
-      break;
+      case githubEventNames.pull_request:
+        icon = githubEventIcons.pull_request;
+        break;
 
-    case githubEventNames.issue:
-      icon = githubEventIcons.issue;
-      break;
+      case githubEventNames.issue:
+        icon = githubEventIcons.issue;
+        break;
+
+      default:
+        icon = githubEventIcons.pull_request;
+    }
+
+    return icon;
   }
 
-  return icon;
-}
+  function createEventSentence(event) {
+    let eventSentence = '';
 
-function createEventSentence(event) {
-  let eventSentence = '';
+    switch (event.event_name) {
+      case githubEventNames.check_suite:
+        const conclusion = event.conclusion === 'success' ? 'passed' : 'failed';
+        const object = event.number ? `pull request #${event.number}` : `branch ${event.head_branch}`;
+        eventSentence = `Checks ${conclusion} for ${object} on ${event.repository}`;
+        break;
 
-  switch (event.event_name) {
-    case githubEventNames.check_suite:
-      const conclusion = event.conclusion === 'success' ? 'passed' : 'failed';
-      const object = event.number ? `pull request #${event.number}` : `branch ${event.head_branch}`;
-      eventSentence = `Checks ${conclusion} for ${object} on ${event.repository}`;
-      break;
+      case githubEventNames.pull_request:
+        const action = event.merged ? 'merged' : event.action;
+        eventSentence = `${event.sender} ${action} pull request #${event.number} on ${event.repository}`;
+        break;
 
-    case githubEventNames.pull_request:
-      const action = event.merged ? 'merged' : event.action;
-      eventSentence = `${event.sender} ${action} pull request #${event.number} on ${event.repository}`;
-      break;
+      case githubEventNames.issue:
+        eventSentence = `${event.sender} ${event.action} issue #${event.number} on ${event.repository}`;
+        break;
+    }
 
-    case githubEventNames.issue:
-      eventSentence = `${event.sender} ${event.action} issue #${event.number} on ${event.repository}`;
-      break;
+    return eventSentence;
   }
-
-  return eventSentence;
-}
-/* 
- PREACT APP 
-*/
+  /* 
+   PREACT APP 
+  */
 
 
-const Placeholder = () => preact.h("li", null, preact.h("img", {
-  src: `${assetsDir}/images/coffee.svg`,
-  height: "15"
-}), "No activity yet!");
-
-const GithubEvent = props => {
-  const event = props.event,
-        html_url = props.event.html_url,
-        flash = props.flash;
-  return preact.h("li", {
-    class: flash ? 'animate' : ''
-  }, preact.h("img", {
-    src: `${assetsDir}/images/${getEventIcon(event)}`,
+  const Placeholder = () => preact.h("li", null, preact.h("img", {
+    src: `${assetsDir}/${imagesDir}/coffee.svg`,
     height: "15"
-  }), preact.h("a", {
-    target: "_blank",
-    href: html_url
-  }, createEventSentence(event)));
-};
+  }), "No activity yet!");
 
-class GithubEventList extends Component {
-  constructor() {
-    super();
-    this.state.latestEvents = [];
-  }
-
-  twitchListenHandler(target, contentType, data) {
-    const event_json = JSON.parse(data);
-    const newEvents = [event_json].concat(this.state.latestEvents);
-    if (newEvents.length > eventListSize) newEvents.pop();
-    this.setState({
-      latestEvents: newEvents,
-      flash: true
-    });
-    setTimeout(() => this.setState({
-      flash: false
-    }), 1000);
-  }
-
-  componentDidMount() {
-    twitch.listen('broadcast', this.twitchListenHandler.bind(this));
-  }
-
-  render(props, state) {
-    const latestEvents = state.latestEvents,
-          flash = state.flash;
-    const list = latestEvents.map((event, index) => {
-      return preact.h(GithubEvent, {
-        flash: index === 0 ? flash : false,
-        event: event
-      });
-    });
-    return preact.h("ul", {
-      class: props.visible ? '' : 'hide'
-    }, list.length ? list : preact.h(Placeholder, null));
-  }
-
-}
-
-;
-
-class App extends Component {
-  constructor() {
-    super();
-    this.state.visible = true;
-  }
-
-  toggleVisibility() {
-    const visible = this.state.visible ? false : true;
-    this.setState({
-      visible
-    });
-  }
-
-  render(props, state) {
-    return preact.h("div", null, preact.h("div", {
-      id: "header"
-    }, preact.h("span", null, "Code Activity"), preact.h("button", {
-      id: "hideButton",
-      onClick: () => this.toggleVisibility(),
-      title: state.visible ? 'hide' : 'show'
+  const GithubEvent = props => {
+    const event = props.event,
+          html_url = props.event.html_url,
+          flash = props.flash;
+    const eventIconSrc = `${assetsDir}/images/${getEventIcon(event)}`;
+    const eventSentence = createEventSentence(event);
+    const listItemClass = flash ? 'animate' : '';
+    return preact.h("li", {
+      class: listItemClass
     }, preact.h("img", {
-      src: `${assetsDir}/images/${state.visible ? 'eye-off' : 'eye'}.svg`,
-      height: "13"
-    }))), preact.h(GithubEventList, {
-      visible: state.visible
-    }));
+      src: eventIconSrc,
+      height: "15"
+    }), preact.h("a", {
+      target: "_blank",
+      href: html_url
+    }, eventSentence));
+  };
+
+  class GithubEventList extends Component {
+    constructor() {
+      super();
+      this.state.latestEvents = [];
+    }
+
+    twitchListenHandler(target, contentType, data) {
+      const event_json = JSON.parse(data);
+      const newEvents = [event_json].concat(this.state.latestEvents);
+      if (newEvents.length > eventListSize) newEvents.pop();
+      this.setState({
+        latestEvents: newEvents,
+        flash: true
+      });
+      setTimeout(() => this.setState({
+        flash: false
+      }), animateTimeout);
+    }
+
+    componentDidMount() {
+      twitch.listen('broadcast', this.twitchListenHandler.bind(this));
+    }
+
+    render(props, state) {
+      const latestEvents = state.latestEvents,
+            flash = state.flash;
+      const shouldFlash = index === 0 ? flash : false;
+      const list = latestEvents.map((event, index) => preact.h(GithubEvent, {
+        flash: shouldFlash,
+        event: event
+      }));
+      return preact.h("ul", {
+        class: props.visible ? '' : 'hide'
+      }, list.length ? list : preact.h(Placeholder, null));
+    }
+
   }
 
-}
-/*
-  FINAL RENDER
-*/
+  ;
+
+  class App extends Component {
+    constructor() {
+      super();
+      this.state.visible = true;
+    }
+
+    toggleVisibility() {
+      const visible = this.state.visible ? false : true;
+      this.setState({
+        visible
+      });
+    }
+
+    render(props, state) {
+      const hideButtonTitle = state.visible ? 'hide' : 'show';
+      const hideButtonSVG = state.visible ? 'eye-off' : 'eye';
+      const hideButtonImageSrc = `${assetsDir}/images/${hideButtonSVG}.svg`;
+      return preact.h("div", null, preact.h("div", {
+        id: "header"
+      }, preact.h("span", null, "Code Activity"), preact.h("button", {
+        id: "hideButton",
+        onClick: () => this.toggleVisibility(),
+        title: hideButtonTitle
+      }, preact.h("img", {
+        src: hideButtonImageSrc,
+        height: "13"
+      }))), preact.h(GithubEventList, {
+        visible: state.visible
+      }));
+    }
+
+  }
+  /*
+    FINAL RENDER
+  */
 
 
-render(preact.h(App, null), document.querySelector('#app'));
+  render(preact.h(App, null), document.querySelector('#app'));
+})();
--- src/viewer.js
+++ src/viewer.js
@@ -1,145 +1,159 @@
-const { h, render, Component } = window.preact; 
-const twitch = window.Twitch.ext;
-const eventListSize = 3;
-const assetsDir = 'assets';
+(function() {
+  const { h, render, Component } = window.preact; 
+  const twitch = window.Twitch.ext;
 
-const githubEventNames = {
-  pull_request: 'pull_request',
-  issue: 'issues',
-  check_suite: 'check_suite'
-};
+  const assetsDir = 'assets';
+  const imagesDir = 'images';
+  const eventListSize = 3;
+  const animateTimeout = 1000;
 
-const githubEventIcons = {
-  pull_request: 'git-pull-request.svg',
-  issue: 'alert-circle.svg',
-  check_pass: 'check-circle.svg',
-  check_fail: 'x-circle.svg'
-};
+  const githubEventNames = {
+    pull_request: 'pull_request',
+    issue: 'issues',
+    check_suite: 'check_suite'
+  };
 
-/* 
- HELPER FUNCTIONS
-*/
-function getEventIcon(event) {
-  let icon;
+  const githubEventIcons = {
+    pull_request: 'git-pull-request.svg',
+    issue: 'alert-circle.svg',
+    check_pass: 'check-circle.svg',
+    check_fail: 'x-circle.svg'
+  };
 
-  switch(event.event_name) {
-    case githubEventNames.check_suite:
-      icon = event.conclusion === 'success' ? githubEventIcons.check_pass : githubEventIcons.check_fail; 
-      break;
-    
-    case githubEventNames.pull_request:
-      icon = githubEventIcons.pull_request;
-      break;
+  /* 
+   HELPER FUNCTIONS
+  */
+  function getEventIcon(event) {
+    let icon;
 
-    case githubEventNames.issue:
-      icon = githubEventIcons.issue;
-      break;
+    switch(event.event_name) {
+      case githubEventNames.check_suite:
+        icon = event.conclusion === 'success' ? githubEventIcons.check_pass : githubEventIcons.check_fail; 
+        break;
+      
+      case githubEventNames.pull_request:
+        icon = githubEventIcons.pull_request;
+        break;
+
+      case githubEventNames.issue:
+        icon = githubEventIcons.issue;
+        break;
+
+      default:
+        icon = githubEventIcons.pull_request;
+    }
+
+    return icon;
   }
 
-  return icon;
-}
+  function createEventSentence(event) {
+    let eventSentence = '';
 
-function createEventSentence(event) {
-  let eventSentence = '';
+    switch(event.event_name) {
+      case githubEventNames.check_suite:
+        const conclusion = (event.conclusion === 'success') ? 'passed' : 'failed';
+        const object = event.number ? `pull request #${event.number}` : `branch ${event.head_branch}`;
+        eventSentence = `Checks ${conclusion} for ${object} on ${event.repository}`;     
+        break;
+      
+      case githubEventNames.pull_request:
+        const action = (event.merged) ? 'merged' : event.action;
+        eventSentence = `${event.sender} ${action} pull request #${event.number} on ${event.repository}`;
+        break;
 
-  switch(event.event_name) {
-   case githubEventNames.check_suite:
-     const conclusion = (event.conclusion === 'success') ? 'passed' : 'failed';
-     const object = event.number ? `pull request #${event.number}` : `branch ${event.head_branch}`;
-     eventSentence = `Checks ${conclusion} for ${object} on ${event.repository}`;     
-     break;
-  
-  case githubEventNames.pull_request:
-    const action = (event.merged) ? 'merged' : event.action;
-    eventSentence = `${event.sender} ${action} pull request #${event.number} on ${event.repository}`;
-    break;
+      case githubEventNames.issue:
+        eventSentence = `${event.sender} ${event.action} issue #${event.number} on ${event.repository}`;
+        break;
+    }
 
-  case githubEventNames.issue:
-    eventSentence = `${event.sender} ${event.action} issue #${event.number} on ${event.repository}`;
-    break;
+    return eventSentence;
   }
 
-  return eventSentence;
-}
+  /* 
+   PREACT APP 
+  */
+  const Placeholder = () => (<li><img src={`${assetsDir}/${imagesDir}/coffee.svg`} height="15" />No activity yet!</li>);
 
-/* 
- PREACT APP 
-*/
-const Placeholder = () => (<li><img src={`${assetsDir}/images/coffee.svg`} height="15" />No activity yet!</li>);
-
-const GithubEvent = (props) => { 
-  const { event, event: { html_url }, flash } = props; 
-
-  return (
-    <li class={flash ? 'animate' : ''}>
-      <img src={`${assetsDir}/images/${getEventIcon(event)}`} height="15"/> 
-      <a target="_blank" href={html_url}>
-        {createEventSentence(event)}
-      </a>
-    </li>
-  )
-};
-
-class GithubEventList extends Component {
-  constructor() {
-    super();
-    this.state.latestEvents = [];
-  }
-
-  twitchListenHandler(target, contentType, data) {
-    const event_json = JSON.parse(data);
-    const newEvents = [event_json].concat(this.state.latestEvents);
-    if (newEvents.length > eventListSize) newEvents.pop();
-    this.setState({latestEvents: newEvents, flash: true});
-    setTimeout(() => this.setState({flash: false}), 1000);
-  }
-
-  componentDidMount() {
-    twitch.listen('broadcast', this.twitchListenHandler.bind(this));
-  }
-
-  render(props, state) {
-    const { latestEvents, flash } = state;
-    const list = latestEvents.map((event, index) => {
-      return <GithubEvent flash={index === 0 ? flash : false} event={event} />
-    });
+  const GithubEvent = (props) => { 
+    const { event, event: { html_url }, flash } = props; 
+    const eventIconSrc = `${assetsDir}/images/${getEventIcon(event)}`;
+    const eventSentence = createEventSentence(event);
+    const listItemClass = flash ? 'animate' : '';
 
     return (
-      <ul class={props.visible ? '' : 'hide'}>
-        {list.length ? list : <Placeholder />}
-      </ul>
-    );
-  }
-};
+      <li class={listItemClass}>
+        <img src={eventIconSrc} height="15"/> 
+        <a target="_blank" href={html_url}>{eventSentence}</a>
+      </li>
+    )
+  };
 
-class App extends Component {
-  constructor() {
-    super();
-    this.state.visible = true;
+  class GithubEventList extends Component {
+    constructor() {
+      super();
+      this.state.latestEvents = [];
+    }
+
+    twitchListenHandler(target, contentType, data) {
+      const event_json = JSON.parse(data);
+      const newEvents = [event_json].concat(this.state.latestEvents);
+      if (newEvents.length > eventListSize) newEvents.pop();
+
+      this.setState({latestEvents: newEvents, flash: true});
+      setTimeout(() => this.setState({flash: false}), animateTimeout);
+    }
+
+    componentDidMount() {
+      twitch.listen('broadcast', this.twitchListenHandler.bind(this));
+    }
+
+    render(props, state) {
+      const { latestEvents, flash } = state;
+      const shouldFlash = index === 0 ? flash : false;
+
+      const list = latestEvents.map((event, index) => (<GithubEvent flash={shouldFlash} event={event} />));
+
+      return (
+        <ul class={props.visible ? '' : 'hide'}>
+          {list.length ? list : <Placeholder />}
+        </ul>
+      );
+    }
+  };
+
+  class App extends Component {
+    constructor() {
+      super();
+      this.state.visible = true;
+    }
+
+    toggleVisibility() {
+      const visible = this.state.visible ? false : true;
+      this.setState({visible});
+    }
+
+    render(props, state) {
+      const hideButtonTitle = state.visible ? 'hide' : 'show';
+      const hideButtonSVG = state.visible ? 'eye-off' : 'eye';
+      const hideButtonImageSrc = `${assetsDir}/images/${hideButtonSVG}.svg`;
+
+      return (
+        <div>
+          <div id='header'>
+            <span>Code Activity</span> 
+            <button id='hideButton' onClick={() => this.toggleVisibility()} title={hideButtonTitle}>
+              <img src={hideButtonImageSrc} height='13'/>
+            </button>
+          </div>  
+          <GithubEventList visible={state.visible}/>
+        </div>
+      ) 
+    }
   }
 
-  toggleVisibility() {
-    const visible = this.state.visible ? false : true;
-    this.setState({visible});
-  }
-
-  render(props, state) {
-    return (
-      <div>
-        <div id='header'>
-          <span>Code Activity</span> 
-          <button id='hideButton' onClick={() => this.toggleVisibility()} title={state.visible ? 'hide' : 'show'}>
-            <img src={`${assetsDir}/images/${state.visible ? 'eye-off' : 'eye'}.svg`} height='13'/>
-          </button>
-        </div>  
-        <GithubEventList visible={state.visible}/>
-      </div>
-    ) 
-  }
-}
-
-/*
-  FINAL RENDER
-*/
-render(<App />, document.querySelector('#app'));
+  /*
+    FINAL RENDER
+  */
+  render(<App />, document.querySelector('#app'));
+}());
 
